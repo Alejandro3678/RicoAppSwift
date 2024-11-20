315d86fd53189b61b41fd3c50a0dde2dda0a4937
Alejandro3678 <carrilloalejandro3897@gmail.com>
2024-11-19 20:26:13 -0800

    Initial Commit

---
+++ RicoAppSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RicoAppSwift
+//
+//  Created by MacOsX on 11/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RicoAppSwift/RicoAppSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  RicoAppSwiftApp.swift
+//  RicoAppSwift
+//
+//  Created by MacOsX on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct RicoAppSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
