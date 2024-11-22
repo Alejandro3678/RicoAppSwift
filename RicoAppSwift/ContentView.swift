9344055166cb811e428c63f92f822e40245deaea
Alejandro3678 <carrilloalejandro3897@gmail.com>
2024-11-21 19:41:22 -0800

    Perfil

--- RicoAppSwift/ContentView.swift
+++ RicoAppSwift/ContentView.swift
@@ -9,8 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("PROYECTO SWIFT - XCODE - JONATHAN DOMINGUEZ")
-            .padding()
+        PerfilInfoView()
     }
 }
 
